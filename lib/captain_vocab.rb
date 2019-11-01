656e05a5075bc9ec7efa3b9d85bf87075a3e7be8
Naya Willis <greedybrain18@gmail.com>
2019-11-01 09:07:09 -0400

    created CLI class

---
+++ lib/captain_vocab.rb
@@ -0,0 +1,6 @@
+require "captain_vocab/version"
+
+module CaptainVocab
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/captain_vocab/cli.rb
@@ -0,0 +1,31 @@
+class CaptainVocab::CLI
+    
+    def call 
+        # intro
+        # prompt user for word
+        # check word
+        # ask another word
+        # goodbye 
+    end
+
+    def intro
+        #
+    end
+
+    def which_word?
+        #
+    end
+
+    def verify_word
+        #   
+    end
+
+    def another_one?
+        #
+    end
+
+    def goodbye 
+        #
+    end
+
+end
