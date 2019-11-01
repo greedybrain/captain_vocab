3e9a58abf8f534bb3f9494a744bee08325b1e645
Naya Willis <greedybrain18@gmail.com>
2019-11-01 09:25:21 -0400

    file changes

---
+++ config/env.rb
@@ -0,0 +1,11 @@
+# LIBRARIES 
+
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+# MAIN ENVIRONMENT
+
+require_relative "../lib/captain_vocab/version"
+require_relative "../lib/captain_vocab/scraper"
+require_relative "../lib/captain_vocab/cli"
---
+++ lib/captain_vocab/scraper.rb
@@ -0,0 +1,11 @@
+require_relative '../../config/env'
+
+class CaptainVocab::Scraper 
+    binding.pry
+    # nokogiri setup to scrape dictionary
+    def scrape_it
+        
+    end
+    # pinpoint data retrieve data
+    # make data accessible to print
+end
